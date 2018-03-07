1ccec0eb5c87d03dfda2623b0a58c6bf9f7a7b37
forel <forel@users.noreply.github.com>
2018-03-07 14:58:26 +0300

    Add project files.

---
+++ DBInventory_Database/dbo/tables/servers_list.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[servers_list](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[server_dnsname] [varchar](255) default '',
+	[server_address] [varchar](64) default '0.0.0.0',
+	[server_description] [varchar](255) default '',
+	constraint pk_server_list primary key (id)
+) ON [PRIMARY]
